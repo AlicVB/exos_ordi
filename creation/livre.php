88e332fae679baaefd62ff46d101ab067ba093af
Aldric Renaudin <alic.vb@gmail.com>
2016-10-10 16:06:03 +0200

    debut modif livre

---
+++ creation/livre.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>exotice -- livres</title>
+  <link rel="stylesheet" href="livre.css">
+</head>
+
+<body>
+  <div class="col">
+    <div class="titre">informations</div>
+    <div class="iligne">
+    Titre du livre :
+    <input type="text" size="30" />
+    </div>
+    <div class="ligne">
+    Couleur de fond :
+    
+    </div>
+  </div>
+  <div class="col">
+    <div class="titre">exercices</div>
+  </div>
+</body>
+</html>
