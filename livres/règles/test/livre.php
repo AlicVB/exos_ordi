5253f1e6b09f58f209d2292de55a29fcb02c49ee
Aldric Renaudin <alic.vb@gmail.com>
2016-10-14 13:20:54 +0200

    livre.php : php->html+php

--- livres/règles/test/livre.php
+++ livres/règles/test/livre.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
   // infos sur le chemin du livre
   $livreid = basename(dirname($_SERVER['PHP_SELF']));
   $cat = basename(dirname(dirname($_SERVER['PHP_SELF'])));
@@ -72,88 +72,74 @@
   $vals = explode("\n", $txt_exo);
   $titre_exo = $vals[0];
   $consigne = $vals[1];
-  
-  //on affiche les en-têtes
-  echo "<!DOCTYPE html>\n";
-  echo "<html><head>\n";
-  echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
-  echo "<title>$titre_livre -- $titre_exo</title>\n";
-
-  // les fichiers à inclure
-  echo "<script type=\"text/javascript\" src=\"$exos[$exo]/exo.js\"></script>\n";
-  echo "<script type=\"text/javascript\" src=\"livre.js\"></script>\n";
-  echo "<link rel=\"stylesheet\" href=\"livre.css\">\n";
-  echo "<link rel=\"stylesheet\" href=\"$root/libs/dragula.min.css\">\n";
-  echo "<link rel=\"stylesheet\" href=\"$exos[$exo]/exo.css\">\n";
-  echo "</head>\n";
-  
-  // on affiche le corps
   $txt = htmlentities(str_replace("\n", "§", addslashes($txt_exo)));
-  echo "<body onload=\"charge('$user', '$livreid', '$exos[$exo]', '$txt', '$root/');\">\n";
-  echo "<script type=\"text/javascript\" src=\"$root/libs/dragula.min.js\"></script>\n";
-  // cadre exercice
-  echo "<div id=\"c1\">\n";
-  echo "<span id=\"user\">Prénom : $user</span>\n";
-  include "$exos[$exo]/exo.php";
-  echo "</div>\n";
-  
-  // cadre à côté
-  echo "<div id=\"c2\">\n";
-  echo "<span id=\"titrelivre\">$titre_livre</span>\n";
-  echo "<br/><span id=\"titreexo\">$titre_exo</span>\n";
-  echo "<br/><div id=\"consigne\">$consigne</div>\n";
-  
-  // aide
-  echo "<br/><div id=\"aideimg\"><img style=\"height: 12vh;\" src=\"$root/icons/help-hint.svg\" onmouseover=\"affiche_aide(true);\" onmouseout=\"affiche_aide(false);\" /></div>\n";
-  
-  // correction
-  echo "<div id=\"corr\"><table id=\"ctable\">\n";
-  echo "<tr><th colspan=\"3\" id=\"cscore\">Sujets : 10/10 -- Verbes : 10/10</th></tr>\n";
-  echo "<tr></tr><th id=\"cflag\"><img id=\"cflagimg\" src=\"$root/icons/flag-red.svg\" /></th>\n";
-  echo "<th id=\"ctxt\">--</th>\n";
-  echo "<th id=\"cbtn\"><img id=\"cbtnimg\" src=\"$root/icons/edit-find-replace.svg\" onclick=\"affiche_score(true);\" /></th>\n";
-  echo "</tr></table></div>";
-  
-  // les numéros de page
-  echo "<div id=\"bas\"\n>\n";
-  if ($lien_pre != "") echo "<a href=\"$lien_pre\"><img style=\"height: 3vh; vertical-align: bottom;\" src=\"$root/icons/go-previous.svg\" /></a>\n";
-  for ($i=0; $i<count($exos); $i++)
-  {
-    $j = $i + 1;
-    $cl = "page";
-    if ($i < $exo) $cl = "pageavant";
-    else if ($i == $exo) $cl = "pagecours";
-    echo "<a class=\"$cl\" href=\"livre.php?user=$user&exo=$i\">$j</a>\n";
-  }
-  echo "<a class=\"pagebilan\" href=\"bilan.php?user=$user\">B</a>\n";
-  if ($lien_next != "") echo "<a href=\"$lien_next\"><img style=\"height: 3vh; vertical-align: bottom;\" src=\"$root/icons/go-next.svg\" /></a>\n";
-  echo "</div>\n";
-  
-  echo "<img id=\"aide\" src=\"$img_livre\"  onload=\"intro_img_load(this)\"/>\n";
-  
-  echo "</div>\n";
-  
-  // retour au sommaire
-  echo "<div id=\"exitdiv\">\n";
-  echo "<a href=\"$root/sommaire.php?user=$user\"><img id=\"exitimg\" src=\"$root/icons/system-shutdown.svg\" /></a>\n";
-  echo "<a id=\"erasea\" href=\"livre.php?user=$user&exo=$exo&erase=1\"><img id=\"eraseimg\" src=\"$root/icons/draw-eraser.svg\" /></a>";
-  echo "</div>\n";
-  
-  // coins page
-  if ($lien_next != "")
-  {
-    echo "<div class=\"coin_d\">\n";
-    echo "<a href=\"$lien_next\"><img id=\"coinimg\" src=\"$root/icons/go-next_coin.svg\" /></a>\n";
-    echo "</div>\n";
-  }
-  //license
-  echo "<div id=\"bysa\"><img src=\"$root/icons/by-sa.png\" />© $aut_livre</div>\n";
-  /*if ($exo_pre >= 0)
-  {
-    echo "<div class=\"coin_g\">\n";
-    echo "<a href=\"livre.php?user=$user&exo=$exo_pre\"><img id=\"coinimg\" src=\"$root/icons/go-previous_coin.svg\" /></a>\n";
-    echo "</div>\n";
-  }*/
-  
-  echo "</body></html>";
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title><?php echo "$titre_livre -- $titre_exo" ?></title>
+    <script type="text/javascript" src="<?php echo $exos[$exo] ?>/exo.js"></script>
+    <script type="text/javascript" src="livre.js"></script>
+    <link rel="stylesheet" href="livre.css">
+    <link rel="stylesheet" href="<?php echo $root ?>/libs/dragula.min.css">
+    <link rel="stylesheet" href="<?php echo $exos[$exo] ?>/exo.css">
+  </head>
+  <body onload="charge('<?php echo $user ?>', '<?php echo $livreid ?>', '<?php echo $exos[$exo] ?>', '<?php echo $txt ?>', '<?php echo $root ?>/');">
+    <script type="text/javascript" src="<?php echo $root ?>/libs/dragula.min.js"></script>
+    <div id="c1">
+      <span id="user">Prénom : <?php echo $user ?></span>
+      <?php include "$exos[$exo]/exo.php" ?>
+    </div>
+    <div id="c2">
+      <span id="titrelivre"><?php echo $titre_livre ?></span>
+      <br/><span id="titreexo"><?php echo $titre_exo ?></span>
+      <br/><div id="consigne"><?php echo $consigne ?></div>
+      <br/>
+      <div id="aideimg">
+        <img style="height: 12vh;" src="<?php echo $root ?>/icons/help-hint.svg" onmouseover="affiche_aide(true);" onmouseout="affiche_aide(false);" />
+      </div>
+      <div id="corr">
+        <table id="ctable">
+          <tr><th colspan="3" id="cscore">Sujets : 10/10 -- Verbes : 10/10</th></tr>
+          <tr>
+            <th id="cflag"><img id="cflagimg" src="<?php echo $root ?>/icons/flag-red.svg" /></th>
+            <th id="ctxt">--</th>
+            <th id="cbtn"><img id="cbtnimg" src="<?php echo $root ?>/icons/edit-find-replace.svg" onclick="affiche_score(true);" /></th>
+          </tr>
+        </table>
+      </div>
+      <div id="bas">
+        <?php
+          if ($lien_pre != "") echo "<a href=\"$lien_pre\"><img style=\"height: 3vh; vertical-align: bottom;\" src=\"$root/icons/go-previous.svg\" /></a>\n";
+          for ($i=0; $i<count($exos); $i++)
+          {
+            $j = $i + 1;
+            $cl = "page";
+            if ($i < $exo) $cl = "pageavant";
+            else if ($i == $exo) $cl = "pagecours";
+            echo "<a class=\"$cl\" href=\"livre.php?user=$user&exo=$i\">$j</a>\n";
+          }
+          echo "<a class=\"pagebilan\" href=\"bilan.php?user=$user\">B</a>\n";
+          if ($lien_next != "") echo "<a href=\"$lien_next\"><img style=\"height: 3vh; vertical-align: bottom;\" src=\"$root/icons/go-next.svg\" /></a>\n";
+        ?>
+      </div>
+      <img id="aide" src="<?php echo $img_livre ?>"  onload="intro_img_load(this)"/>
+    </div>
+    <div id="exitdiv">
+      <a href="<?php echo $root ?>/sommaire.php?user=<?php echo $user ?>"><img id="exitimg" src="<?php echo $root ?>/icons/system-shutdown.svg" /></a>
+      <a id="erasea" href="livre.php?user=<?php echo $user ?>&exo=<?php echo $exo ?>&erase=1"><img id="eraseimg" src="<?php echo $root ?>/icons/draw-eraser.svg" /></a>
+    </div>
+    <?php
+      // coins page
+      if ($lien_next != "")
+      {
+        echo "<div class=\"coin_d\">\n";
+        echo "<a href=\"$lien_next\"><img id=\"coinimg\" src=\"$root/icons/go-next_coin.svg\" /></a>\n";
+        echo "</div>\n";
+      }
+    ?>
+    <div id="bysa"><img src="<?php echo $root ?>/icons/by-sa.png" />© <?php echo $aut_livre ?></div>;
+  </body>
+</html>
